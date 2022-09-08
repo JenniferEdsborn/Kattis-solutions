4f389941f04646db4567f7c87a68009beb280b58
Jennifer <ki.in@live.se>
2022-09-08 11:38:12 +0200

    ok

---
+++ FYI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FYI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int key = 5;
+
+            int firstDigit = int.Parse(number.ToString().Substring(0, 1));
+            int secondDigit = int.Parse(number.ToString().Substring(1, 1));
+            int thirdDigit = int.Parse(number.ToString().Substring(2, 1));
+
+            if (firstDigit == key && secondDigit == key && thirdDigit == key)
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+
+        }
+    }
+}
